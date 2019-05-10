1c3aec95f76025d183c9ba1a5fd8b7d4bf50d5b7
mikestratton <mike@mikestratton.net>
2019-05-10 03:27:35 -0400

    laravel service provider autoload from composer.json

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Mikestratton\\Hello\\' => array($baseDir . '/src'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit9a7e58caf5a527cde3daae752390b5a6
 {
+    public static $prefixLengthsPsr4 = array (
+        'M' => 
+        array (
+            'Mikestratton\\Hello\\' => 19,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Mikestratton\\Hello\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/src',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit9a7e58caf5a527cde3daae752390b5a6::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit9a7e58caf5a527cde3daae752390b5a6::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
